4be7a934259c9e6415b43dc26e4790e4529e19d2
BrunoCombal <bruno.combal@gmail.com>
2013-12-04 15:17:24 +0100

    better parameterized version. Now code 1.e20 on an integer, so that gdal_rasterize can cast it from text to float!

--- run.sh
+++ run.sh
@@ -3,11 +3,19 @@
 # \author: Bruno Combal, IOC/UNESCO
 # \date: 2013, December
 
-mkdir /data/tmp/new_algo/export_tif
+indir=/data/tmp/new_algo/dhm_rcprcp85/
+outdir=/data/tmp/new_algo/export_tif
+maskSHP=/data/tmp/new_algo/world.shp
+mkdir -p $outdir
+tmpdir=$outdir/tmp
+mkdir -p $tmpdir
 
-./ncToGTiff.sh -o /data/tmp/new_algo/export_tif/frequency_lvl2_2050.tif -d lvl2_freq -w tmp /data/tmp/new_algo/dhm_rcprcp85/frequency_lvl2_2050.nc 
+nodata=-1
+./ncToGTiff.sh -o ${outdir}/frequency_lvl2_2050.tif -n $nodata -m ${maskSHP} -d lvl2_freq -w ${tmpdir} ${indir}/frequency_lvl2_2050.nc 
 
-./ncToGTiff.sh -o /data/tmp/new_algo/export_tif/dhm_decade_2050.tif -d dhm -w tmp -p /data/tmp/new_algo/dhm_rcprcp85/ `for ((ii=2050; ii<2060; ii++)); do echo dhm_${ii}.nc; done`
+# gdalrasterize does not understand 1.e20
+nodata=100000000000000000000
+./ncToGTiff.sh -w ${tmpdir} -o ${outdir}/dhm_decade_2050.tif -n $nodata -d dhm -w tmp -m ${maskSHP} -p ${indir} `for ((ii=2050; ii<2060; ii++)); do echo dhm_${ii}.nc; done`
 
 
 # end of script
