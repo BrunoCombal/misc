5d44a86cd2a7af6f362091df6ec52165db67d61e
BrunoCombal <bruno.combal@gmail.com>
2014-02-10 16:41:37 +0100

    has a parameter to define the decade to convert to GTiff

--- run.sh
+++ run.sh
@@ -6,16 +6,18 @@
 indir=/data/tmp/new_algo/dhm_rcprcp85/
 outdir=/data/tmp/new_algo/export_tif
 maskSHP=/data/tmp/new_algo/landMask.shp
+decade=2030
+decadeEnd=$((decade+10))
 mkdir -p $outdir
 tmpdir=$outdir/tmp
 mkdir -p $tmpdir
 
 nodata=-1
-./ncToGTiff.sh -o ${outdir}/frequency_lvl2_2050.tif -n $nodata -m ${maskSHP} -d lvl2_freq -w ${tmpdir} ${indir}/frequency_lvl2_2050.nc 
+./ncToGTiff.sh -o ${outdir}/frequency_lvl2_${decade}.tif -n $nodata -m ${maskSHP} -d lvl2_freq -w ${tmpdir} ${indir}/frequency_lvl2_${decade}.nc 
 
 # gdalrasterize does not understand 1.e20
 nodata=100000000000000000000
-./ncToGTiff.sh -w ${tmpdir} -o ${outdir}/dhm_decade_2050.tif -n $nodata -d dhm -w tmp -m ${maskSHP} -p ${indir} `for ((ii=2050; ii<2060; ii++)); do echo dhm_${ii}.nc; done`
+./ncToGTiff.sh -w ${tmpdir} -o ${outdir}/dhm_decade_${decade}.tif -n $nodata -d dhm -w tmp -m ${maskSHP} -p ${indir} `for ((ii=${decade}; ii<${decadeEnd}; ii++)); do echo dhm_${ii}.nc; done`
 
 
 # end of script
