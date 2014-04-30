6d676f05e242b5e6734df7bd06c7bba395b645f0
BrunoCombal <bruno.combal@gmail.com>
2014-04-30 16:40:17 +0200

    no significant change, just for production

--- run.sh
+++ run.sh
@@ -3,21 +3,30 @@
 # \author: Bruno Combal, IOC/UNESCO
 # \date: 2013, December
 
-indir=/data/tmp/new_algo/dhm_rcprcp85/
+rcp='rcp85'
+
+indir='/data/tmp/new_algo/dhm_rcp'${rcp}'/'
 outdir=/data/tmp/new_algo/export_tif
 maskSHP=/data/tmp/new_algo/landMask.shp
-decade=2030
-decadeEnd=$((decade+10))
 mkdir -p $outdir
 tmpdir=$outdir/tmp
 mkdir -p $tmpdir
 
-nodata=-1
-./ncToGTiff.sh -o ${outdir}/frequency_lvl2_${decade}.tif -n $nodata -m ${maskSHP} -d lvl2_freq -w ${tmpdir} ${indir}/frequency_lvl2_${decade}.nc 
 
-# gdalrasterize does not understand 1.e20
-nodata=100000000000000000000
-./ncToGTiff.sh -w ${tmpdir} -o ${outdir}/dhm_decade_${decade}.tif -n $nodata -d dhm -w tmp -m ${maskSHP} -p ${indir} `for ((ii=${decade}; ii<${decadeEnd}; ii++)); do echo dhm_${ii}.nc; done`
+for decade in 2030 2040 2050
+do
+    nodata=-1
+    echo 'frequency: nc to gtiff'
+    ./ncToGTiff.sh -o ${outdir}/frequency_lvl2_${decade}_${rcp}.tif  -n $nodata -m ${maskSHP} -d lvl2_freq -w ${tmpdir} ${indir}/frequency_lvl2_${decade}.nc 
+
+#    echo "dhm: nc to gtiff"
+    # gdalrasterize does not understand 1.e20
+#    nodata=100000000000000000000
+#    decadeEnd=$((decade+10))
+#    ./ncToGTiff.sh -w ${tmpdir} -o ${outdir}/dhm_decade_${decade}_${rcp}.tif -n $nodata -d dhm -w tmp -m ${maskSHP} -p ${indir} `for ((ii=${decade}; ii<${decadeEnd}; ii++)); do echo dhm_${ii}.nc; done`
+
+done
+
 
 
 # end of script
